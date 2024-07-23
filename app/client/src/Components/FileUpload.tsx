8872de660e9c28350c6c153bad1deca5b30e5087
BennyKitchell <ben@Bens-MacBook-Air.local>
2024-07-22 19:36:18 -0500

    feat: fetch the response and siaply it

--- app/client/src/Components/FileUpload.tsx
+++ app/client/src/Components/FileUpload.tsx
@@ -1,19 +1,47 @@
 import React, { useState } from "react";
+import FileDisplay from "./FileDisplay";
 
 function FileUpload() {
   const [fileSelected, setFileSelected] = useState<File>();
+  const [commands, setCommands] = useState<String[]>([]);
 
-  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files !== null) {
       setFileSelected(event.target.files[0]);
     }
   }
+
+  const fetchData = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
+    e.preventDefault();
+    if (fileSelected) {
+      const data = new FormData();
+      data.append("", fileSelected, fileSelected.name);
+
+      fetch("http://localhost:3000/upload/file", {
+        method: "POST",
+        body: data,
+      })
+        .then((response) => {
+          return response.json();
+        })
+
+        .then((data) => {
+          setCommands(data);
+        });
+    }
+  };
+  
   return (
     <>
       <form encType="multipart/form-data" action="">
-        <div>{JSON.stringify(fileSelected)}</div>
+        <FileDisplay commands={commands}/>
         <input type="file" onChange={handleChange} />
-        <input className={"button"} type="button" value="upload"></input>
+        <input
+          className={"button"}
+          type="button"
+          value="upload"
+          onClick={fetchData}
+        ></input>
       </form>
     </>
   );
