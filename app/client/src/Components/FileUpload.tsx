494333a442deac1bcea561a2766a8dc666075221
BennyKitchell <ben@Bens-MacBook-Air.local>
2024-07-22 19:24:26 -0500

    chore: handle file change event

--- app/client/src/Components/FileUpload.tsx
+++ app/client/src/Components/FileUpload.tsx
@@ -1,8 +1,18 @@
+import React, { useState } from "react";
+
 function FileUpload() {
+  const [fileSelected, setFileSelected] = useState<File>();
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    if (event.target.files !== null) {
+      setFileSelected(event.target.files[0]);
+    }
+  }
   return (
     <>
       <form encType="multipart/form-data" action="">
-        <input type="file" />
+        <div>{JSON.stringify(fileSelected)}</div>
+        <input type="file" onChange={handleChange} />
         <input className={"button"} type="button" value="upload"></input>
       </form>
     </>
