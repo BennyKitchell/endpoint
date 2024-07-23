7accec4f43d589c076a49d588c2189a6369f7ada
BennyKitchell <ben@Bens-MacBook-Air.local>
2024-07-22 19:55:26 -0500

    feat: Display the file output in a formatted way

--- app/client/src/Components/FileDisplay.tsx
+++ app/client/src/Components/FileDisplay.tsx
@@ -1,10 +1,34 @@
+import parse from "html-react-parser";
+
 interface DisplayProps {
-  commands: String[];
+  commands: string[];
 }
+
 function FileDisplay(props: DisplayProps) {
+  const commands = props.commands;
   return (
     <>
-      <div>{props.commands}</div>
+      {
+        <div>
+          {commands.length > 0 ? (
+            commands.map((notification: string, key: number) => (
+              <div key={key} className="outer-command-container">
+                <div
+                  className={
+                    notification.includes("Invalid")
+                      ? "command error"
+                      : "command"
+                  }
+                >
+                  {parse(notification)}
+                </div>
+              </div>
+            ))
+          ) : (
+            <h2>Please upload a file below</h2>
+          )}
+        </div>
+      }
     </>
   );
 }
